b74ab430feb301ca202622846fa7e590f4711fd9
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-04 20:39:18 -0500

    Update index.js
    Linter errors fix in script.js.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const hamburger = document.querySelector(".hamburguer");
-const navMenu = document.querySelector("nav");
-const navItems = document.querySelectorAll(".item-nav2");
+const hamburger = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('nav');
+const navItems = document.querySelectorAll('.item-nav2');
 
-hamburger.addEventListener("click", ()=> {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
 navItems.forEach((navItem) => {
-    navItem.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    });
+  navItem.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  });
 });
