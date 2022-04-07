2f982ffa521189dbb349ec89a633e89eadf3042a
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-07 16:39:12 -0500

    Update form-data.js
    Fix linter error in form-data.js.

--- js/form-data.js
+++ js/form-data.js
@@ -18,7 +18,7 @@ function setFormInput(inputType) {
 }
 
 window.addEventListener('load', () => {
-  if (localStorage.getItem("form-data") === null) {
+  if (localStorage.getItem('form-data') === null) {
     localStorage.setItem('form-data', JSON.stringify(formData));
   } else {
     formData = JSON.parse(localStorage.getItem('form-data'));
