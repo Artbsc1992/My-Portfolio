c9e77110af78353e0d237ee4b425da6e887a32cb
Arturo <ahpareja@gmail.com>
2022-04-07 10:49:39 -0500

    Add new error message

--- js/form-validation.js
+++ js/form-validation.js
@@ -4,7 +4,7 @@ smallMsg.classList.add('form-response');
 
 const nameErrorMsg = 'Name is required!';
 const emailReqMsg = 'Email is required!';
-const emailInvMsg = 'Email is invalid!';
+const emailInvMsg = 'Email is invalid! please use lowercase and format xxx@xxx.xx';
 const commentsReqMsg = 'What are you trying to say?';
 const commentsInvMsg = 'Your message is too long or too short!';
 
@@ -16,48 +16,43 @@ function validateName(name, reqmsg) {
 }
 
 function validateEmail(email, reqmsg, invmsg) {
-  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
   if (email.length > 5) {
     if (emailRegex.test(email.trim()) && email.toLowerCase() === email) {
       return [true, ''];
-    } else {
-      return [false, invmsg];
     }
-  } else {
-    return [false, reqmsg];
+    return [false, invmsg];
   }
+  return [false, reqmsg];
 }
 
 function validateComments(comments, reqmsg, invmsg) {
   if (comments.length > 0) {
-    if (10 <= comments.length && comments.length <= 512) {
+    if (comments.length >= 10 && comments.length <= 512) {
       return [true, ''];
-    } else {
-      return [false, invmsg];
     }
-  } else {
-    return [false, reqmsg];
+    return [false, invmsg];
   }
+  return [false, reqmsg];
 }
 
-form.addEventListener("submit", (event) => {
-	let nameValid = validateName(form.elements["full-name"].value, nameErrorMsg);
-	let emailValid = validateEmail(form.elements.email.value, emailReqMsg, emailInvMsg);
-  let commentsValid = validateComments(form.elements.msg.value, commentsReqMsg, commentsInvMsg);
-	if (nameValid[0] && emailValid[0] && commentsValid[0]) {
+form.addEventListener('submit', (event) => {
+  const nameValid = validateName(form.elements['full-name'].value, nameErrorMsg);
+  const emailValid = validateEmail(form.elements.email.value, emailReqMsg, emailInvMsg);
+  const commentsValid = validateComments(form.elements.msg.value, commentsReqMsg, commentsInvMsg);
+  if (nameValid[0] && emailValid[0] && commentsValid[0]) {
     smallMsg.innerHTML = 'Yay, thanks for contacting me!';
 
-    form.elements["full-name"].classList.remove('invalid-state');
-    form.elements["email"].classList.remove('invalid-state');
-    form.elements["msg"].classList.remove('invalid-state-comments');
+    form.elements['full-name'].classList.remove('invalid-state');
+    form.elements.email.classList.remove('invalid-state');
+    form.elements.msg.classList.remove('invalid-state-comments');
     smallMsg.classList.remove('unsuccess');
 
-    form.elements["full-name"].classList.add('valid-state');
-    form.elements["email"].classList.add('valid-state');
-    form.elements["msg"].classList.add('valid-state-comments');
+    form.elements['full-name'].classList.add('valid-state');
+    form.elements.email.classList.add('valid-state');
+    form.elements.msg.classList.add('valid-state-comments');
     smallMsg.classList.add('success');
-    return;
-	} else {
+  } else {
     smallMsg.classList.remove('success');
     smallMsg.classList.add('unsuccess');
     event.preventDefault();
